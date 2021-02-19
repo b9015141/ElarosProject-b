e7678da9e336c662f1eda0b5bb9127c943e3f593
Soraya Charef <60435084+b9015141@users.noreply.github.com>
2021-02-19 14:15:05 +0000

    Add files via upload

--- ElarosProject/patient/dashboard.php
+++ ElarosProject/patient/dashboard.php
@@ -1,7 +1,5 @@
 <?php
-    session_start();
     include_once '../inc/connection.php';
-    include_once '...inc/loginServer.php';
 ?>
 <!DOCTYPE html>
 <html>
@@ -16,32 +14,12 @@
     <script type="text/javascript" src="/scripts/jquery-3.4.1.js"></script>  
     <script src='/scripts/script.js' type="text/javascript"></script>
     <script src='/scripts/fullpage.js' type="text/javascript"></script>
-    <script src="https://cdnjs.cloudflare.com/ajax/libs/Chart.js/2.1.3/Chart.min.js"></script>
     <meta name="viewport" content="width=device-width, initial-scale=1">
 
     <title>Dashboard</title>
     <link href="/images/elaros.jpg" rel="icon"/>
     
 </head>
-<?php
-
-$name = $_SESSION['user'];
-$sql = "SELECT * FROM answers WHERE patientName = '$name'";
-$result = $conn->query($sql);
-
-
-while ($row = mysqli_fetch_array($result)){
-   $pre = $pre.'"'.$row['q1aPre'].'"'.','.'"'.$row['q4bPre'].'"'.','.'"'.$row['q6bPre'].'"'.','.'"'.$row['q8aPre'].'"'.','.'"'.$row['q9aPre'].'"'.','.'"'.$row['q12aPre'].'"'.','.'"'.$row['q13aPre'].'"'.','.'"'.$row['q14aPre'].'"'.','.'"'.$row['q15aPre'].'"';
-   
-   $now = $now.'"'.$row['q1aNow'].'"'.','.'"'.$row['q4bNow'].'"'.','.'"'.$row['q6bNow'].'"'.','.'"'.$row['q8aNow'].'"'.','.'"'.$row['q9aNow'].'"'.','.'"'.$row['q12aNow'].'"'.','.'"'.$row['q13aNow'].'"'.','.'"'.$row['q14aNow'].'"'.','.'"'.$row['q15aNow'].'"';
-}
-
-
-
-
-
-?>
-
 <body>
     <nav>
         <div class="logo">
@@ -49,7 +27,6 @@ while ($row = mysqli_fetch_array($result)){
         </div>
         <ul class="nav-links">
             <li><a href="home.php">HOME</a></li>
-            <li><a href="questionnaire.php">QUESTIONNAIRE</a></li>
             <li><a href="dashboard.php">DASHBOARD</a></li>
             <li><a href="../Login.php">LOGOUT</a></li>
         </ul>
@@ -60,42 +37,140 @@ while ($row = mysqli_fetch_array($result)){
         </div>
     </nav> 
     
-<canvas id="chart" width="400" height="400"></canvas>
+    <form method="post" action="../inc/export.php">
+        <input type="submit" name="export" id="export" value="CSV Export"/>
+    </form>
+    
+    <h5>Add New Patient</h5>
+
+    <div class="tab">
+        <button class="tablinks" onclick="openCity(event, 'Overview')">Overview</button>
+        <button class="tablinks" onclick="openCity(event, 'Personal')">Personal</button>
+    </div>
+
+    <form id= "dashboardForm" action = "../inc/insertPatient.php" method="POST">
+
+        <div id="Overview" class="tabcontent">
+            <div class = "patientForm">
+                <h2>Overview Form</h2>
+                <label for="fname" name="fnamelabel">First Name</label>
+                <input type="text" name = "firstname" required><br>
+                <label for="lname"name="lnamelabel">Last Name</label>
+                <input type="text" name = "lastname" required><br>
+                <label for="nhsnum"name="nhslabel">NHS Number</label>
+                <h8>Format: 123-645-6478</h8>
+                <input type="tel" name = "nhsnumber"  pattern="[0-9]{3}-[0-9]{3}-[0-9]{4}" required>
+                <label for="language"name="languagelabel">Language</label>
+                <select name="language">
+                    <option value="English">English</option>
+                    <option value="French">French</option>
+                    <option value="German">German</option>
+                    <option value="Italian">Italian</option>
+                    <option value="Spanish">Spanish</option>
+                </select>
+                <label for="ethnicity"name="ethnicitylabel">Ethnicity</label>
+                 <select name="ethnicity">
+                    <option value="White">White</option>
+                    <option value="Asian">Asian</option>
+                    <option value="African">African</option>
+                </select>
+                <label for="gender"name="genderlabel">Gender</label>
+                <input type="radio" name="gender" value="male" required>
+                <label for="gender"name="male">male</label>
+                <input type="radio"  name="gender" value="female" required>
+                <label for="gender"name="female">Female</label>
+                <input type="date" name = "dateofbirth"  required>
+                <input type="submit" class="tablinks" onclick="openCity(event, 'Personal')" value="Save" name="submitOverview">
+
+            </div>
+        </div>
+
+        <div id="Personal" class="tabcontent">
+            <div class="personalForm">
+                <h2>Personal Form</h2>
+                <label for="height">Height (CM)</label>
+                <input type="number" name = "height" required>
+                <label for="weight">Weight (KG)</label>
+                <input type="text" name = "weight" required>
+                <label for="smoker">Smoker</label>
+                <select name="smoker">
+                    <option value="none">Never</option>
+                    <option value="none">occasionally</option>
+                    <option value="none">Daily</option>
+                </select>
+                <label for ="medicalhistory">Medical History</label>
+                <label for ="medicalhistorySet">None Set</label>
+                <label for="street">Street</label>
+                <input type="text" name = "street" required>
+                <label for="city">City</label>
+                <input type="text" name = "city" required>
+                <label for="postcode">Postcode</label>
+                <input type="text" name = "postcode" required>
+                <label for="telephone">Telephone</label>
+                <input type="tel" name = "phone" required>
+                <label for="email">Email</label>
+                <input type="email" name = "email" required> 
+                <input type="submit" value="Save" name="submitPersonal">
+            </div> 
+        </div>
+
+    </form>
+
+<script src="/scripts/nav.js"></script>
+
 <script>
-var ctx = document.getElementById('chart').getContext('2d');
-var myChart = new Chart(ctx, {
-    type: 'radar',
-    data: {
-        labels: ['Breathlessness', 'Cough/throat sensitivity/voice change','Fatigue', 'Pain/Discomfort', 'Anxiety','Depression','Mobility','Personal Care','Other Activities of Daily Living','Social Role'],
-        datasets: [
-            {
-            label: 'Pre-covid',
-            data: [<?php echo $pre;?> ],
-            backgroundColor: 'rgba(255, 99, 132, 0.2)',
-            borderColor: 'rgba(255, 99, 132, 1)',
-            borderWidth: 1
-        },{
-            label: 'Now',
-            data: [<?php echo $now;?> ],
-            backgroundColor: 'rgba(179,181,198,0.2)',
-            borderColor: 'rgba(179,181,198,1)',
-            borderWidth: 1
-        }
-        ]
-    },
-    options: {
-        scales: {
-            yAxes: [{
-                ticks: {
-                    beginAtZero: true
-                }
-            }]
-        }
-    }
-});
+function openCity(evt, cityName) {
+  var i, tabcontent, tablinks;
+  tabcontent = document.getElementsByClassName("tabcontent");
+  for (i = 0; i < tabcontent.length; i++) {
+    tabcontent[i].style.display = "none";
+  }
+  tablinks = document.getElementsByClassName("tablinks");
+  for (i = 0; i < tablinks.length; i++) {
+    tablinks[i].className = tablinks[i].className.replace(" active", "");
+  }
+  document.getElementById(cityName).style.display = "block";
+  evt.currentTarget.className += " active";
+}
 </script>
+   
+   <h5>View Patients</h5>
+   
+    <table id="patientTable">
+        <tr>
+            <th>NHS Number</th>
+            <th>Name</th> 
+            <th>Age (D.O.B)</th>  
+            <th>Ethnicity</th>  
+            <th>Gender</th>
+            <th>Added</th>
+        <tr>
+        
+        <?php
+            $sql = "SELECT * from patients";
+            $result = $conn->query($sql);
 
 
+            if ($result-> num_rows > 0){
+                while ($row = $result-> fetch_assoc()){
+                    $today = date("Y-m-d");
+                    $dateofbirth = $row['dateofbirth'];
+                    $diff = date_diff(date_create($dateofbirth),date_create($today));
+                    $age = $diff->format('%y');
+                    $dob = $age." "."(".$dateofbirth.")";
+                    $name = $row['lastname'].", ".$row['firstname'];
+                    
+                   
+
+                    
+                    echo "<tr><td>" . $row['nhsnumber'] . "</td><td>" . $row['patientName']. "</td><td>". $dob ."</td><td>". $row['ethnicity'] ."</td><td>". $row['gender'] ."</td><td>".$row[dateAdded]. "</td></tr>";  
+                }
+            }
+
+            mysql_close(); 
+        ?>
+    </table>
+    
 </body>
 </html>
 
